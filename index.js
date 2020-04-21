22aac6e41ad245a022170d87976069085b45c60d
yugei806 <sennpuuki810@gmaile.com>
2020-04-21 13:23:08 +0000

    寿司ピザアンケートの追加

--- index.js
+++ index.js
@@ -22,7 +22,12 @@ const server = http.createServer((req, res) => {
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
-      }
+      }else if(req.url === '/enquetes/sushi-pizza'){
+        res.write(pug.renderFile('./form.pug',{
+          path:req.ulr,
+          firstItem:'寿司',
+          secondItem:'ピザ'
+        }))};
       res.end();
       break;
     case 'POST':
